72e7948f51181ae98243fb29c390a1411e5a4e56
shoockrates <146773354+shoockrates@users.noreply.github.com>
2024-12-31 23:20:33 +0200

    Minor formatting and optimization update

--- module/csv_parser.c
+++ module/csv_parser.c
@@ -9,56 +9,63 @@ void cb_field(void *field, size_t field_len, void *data)
 {
     if (!tempPlayer)
     {
-        tempPlayer = malloc(sizeof(PlayerStats));
+        if ((tempPlayer = malloc(sizeof(PlayerStats))) == NULL)
+        {
+            perror("Failed to allocate memory for PlayerStats");
+            exit(-1);
+        }
     }
     if (!tempMatch)
     {
-        tempMatch = malloc(sizeof(MatchStats));
+        if ((tempMatch = malloc(sizeof(MatchStats))) == NULL)
+        {
+            perror("Failed to allocate memory for MatchStats");
+            exit(-1);
+        }
     }
 
     char buffer[256];
-
     snprintf(buffer, field_len + 1, "%.*s", (int)field_len, (char *)field);
 
     switch (fieldIndex)
     {
-    case FIELD_IP_ADDRESS:
-        strncpy(tempPlayer->ipAddress, buffer, sizeof(tempPlayer->ipAddress) - 1);
-        break;
-    case FIELD_PLAYER_NAME:
-        strncpy(tempPlayer->playerName, buffer, sizeof(tempPlayer->playerName) - 1);
-        break;
-    case FIELD_ELAPSED_SECONDS:
-        tempPlayer->elapsedSeconds = atoi(buffer);
-        break;
-    case FIELD_TIMESTAMP:
-        strncpy(tempMatch->timestamp, buffer, sizeof(tempMatch->timestamp) - 1);
-        strncpy(tempPlayer->lastPlayed, buffer, sizeof(tempPlayer->lastPlayed) - 1);
-        break;
-    case FIELD_GAME_WON:
-        tempMatch->gameWon = atoi(buffer);
-        break;
-    case FIELD_BOARD_SIZE:
-        tempMatch->boardSize = atoi(buffer);
-        break;
-    case FIELD_MINE_PERCENTAGE:
-        tempMatch->minePercentage = atof(buffer);
-        break;
-    default:
-        if (fieldIndex >= FIELD_BOARD_FLAT_START && fieldIndex < FIELD_REVEALED_FLAT_START)
-        {
-            tempMatch->boardFlat[fieldIndex - FIELD_BOARD_FLAT_START] = atoi(buffer);
-        }
-        else if (fieldIndex >= FIELD_REVEALED_FLAT_START && fieldIndex < FIELD_FLAGS_FLAT_START)
-        {
-            tempMatch->revealedFlat[fieldIndex - FIELD_REVEALED_FLAT_START] = atoi(buffer);
-        }
-        break;
+        case FIELD_IP_ADDRESS:
+            strncpy(tempPlayer->ipAddress, buffer, sizeof(tempPlayer->ipAddress) - 1);
+            break;
+        case FIELD_PLAYER_NAME:
+            strncpy(tempPlayer->playerName, buffer, sizeof(tempPlayer->playerName) - 1);
+            break;
+        case FIELD_ELAPSED_SECONDS:
+            tempPlayer->elapsedSeconds = atoi(buffer);
+            break;
+        case FIELD_TIMESTAMP:
+            strncpy(tempMatch->timestamp, buffer, sizeof(tempMatch->timestamp) - 1);
+            strncpy(tempPlayer->lastPlayed, buffer, sizeof(tempPlayer->lastPlayed) - 1);
+            break;
+        case FIELD_GAME_WON:
+            tempMatch->gameWon = atoi(buffer);
+            break;
+        case FIELD_BOARD_SIZE:
+            tempMatch->boardSize = atoi(buffer);
+            break;
+        case FIELD_MINE_PERCENTAGE:
+            tempMatch->minePercentage = atof(buffer);
+            break;
+        default:
+            if (fieldIndex >= FIELD_BOARD_FLAT_START && fieldIndex < FIELD_REVEALED_FLAT_START)
+            {
+                tempMatch->boardFlat[fieldIndex - FIELD_BOARD_FLAT_START] = atoi(buffer);
+            }
+            else if (fieldIndex >= FIELD_REVEALED_FLAT_START && fieldIndex < FIELD_FLAGS_FLAT_START)
+            {
+                tempMatch->revealedFlat[fieldIndex - FIELD_REVEALED_FLAT_START] = atoi(buffer);
+            }
+            break;
     }
-    fieldIndex++;
+    ++fieldIndex;
 }
 
-void cb_row(int c, void *data)
+void cb_row(int columnCount, void *data)
 {
     if (tempPlayer && tempMatch && fieldIndex == FIELD_NUM)
     {
@@ -66,17 +73,14 @@ void cb_row(int c, void *data)
     }
     else
     {
-        printf("Ignoring line. The number of columns is incorrect.\n");
+        fprintf(stderr, "Ignoring line. The number of columns is incorrect.\n");
     }
-    if (tempPlayer)
-    {
-        free(tempPlayer);
-        tempPlayer = NULL;
-    }
-    if (tempMatch)
-    {
-        free(tempMatch);
-        tempMatch = NULL;
-    }
+
+    free(tempPlayer);
+    tempPlayer = NULL;
+
+    free(tempMatch);
+    tempMatch = NULL;
+
     fieldIndex = 0;
 }
